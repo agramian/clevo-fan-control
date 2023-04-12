6ba69c6eb7fb5f06106cd4da0541796c247192ce
COMEXR B.V <64539804+comexr@users.noreply.github.com>
2023-04-12 11:59:11 -0400

    Add files via upload

---
+++ faninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt remove -y libayatana-appindicator3-1
+sudo apt install -y gcc cmake libappindicator3-dev libgtk-3-dev pkg-config git
+git clone https://github.com/comexr/fan-control.git
+mkdir fan
+cd fan
+cmake -DUSER=$(whoami) -DGROUP=$(whoami) -DHOME=/home/$(whoami) ~/clevo-fan-control
+cmake --build .
+sudo cmake --install .
+reboot
