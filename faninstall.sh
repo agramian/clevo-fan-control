54f7596ef1adbe96f1e98d933445a7e28dd1b8a6
COMEXR B.V <64539804+comexr@users.noreply.github.com>
2023-04-13 13:11:10 +0200

    Update faninstall.sh

--- faninstall.sh
+++ faninstall.sh
@@ -5,7 +5,7 @@ sudo apt install -y gcc cmake libappindicator3-dev libgtk-3-dev pkg-config git
 git clone https://github.com/comexr/fan-control.git
 mkdir fan
 cd fan
-cmake -DUSER=$(whoami) -DGROUP=$(whoami) -DHOME=/home/$(whoami) ~/clevo-fan-control
+cmake -DUSER=$(whoami) -DGROUP=$(whoami) -DHOME=/home/$(whoami) ~/fan-control
 cmake --build .
 sudo cmake --install .
 reboot
